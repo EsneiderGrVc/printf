4f62abf08454de95229a89bb64d146fc3d9e5ece
sneiergv <sneidergv@gmail.com>
2021-03-20 17:50:46 -0500

    Link with get_op_function fixed

--- holberton.h
+++ holberton.h
@@ -23,6 +23,8 @@ int _printf(const char *format, ...);
 int (*get_specifier_function(const char *format))(va_list);
 int print_c(va_list c);
 int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
 
 int len_number(int n, int len);
 
